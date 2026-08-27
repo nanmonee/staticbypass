ee179eaedbade6a0fc3fb21dff29dda75735e75e
Bill Chenyoung <bchenyoung@gmail.com>
2026-08-27 10:26:41 +1000

    Added environment variables to cs compiler

--- cs/utils/compiler.py
+++ cs/utils/compiler.py
@@ -1,5 +1,6 @@
 import subprocess
 import shutil
+import os
 
 def compile(code: str, output: str, compilerOptions: list[str]) -> str:
     if output[-4:] == '.exe':
@@ -11,10 +12,12 @@ def compile(code: str, output: str, compilerOptions: list[str]) -> str:
         sourcefile = f'{output}/Program.cs'
         outfile = f'{output}.exe'
     shutil.rmtree(sourcefolder, ignore_errors=True)
-    result = subprocess.run(['dotnet', 'new', 'console', '-o', sourcefolder], check=True)
+    custom_env = os.environ.copy()
+    custom_env['TERM'] = 'xterm'
+    result = subprocess.run(['dotnet', 'new', 'console', '-o', sourcefolder],env=custom_env, check=True)
     open(sourcefile,'w').write(code)
     print(f'Writing source code to {sourcefile}')
-    result = subprocess.run(['dotnet', 'publish', '-c', 'Release', '-r','win-x64', '--self-contained', 'true', '-p:PublishSingleFile=true'], cwd=sourcefolder, check=True)
+    result = subprocess.run(['dotnet', 'publish', '-c', 'Release', '-r','win-x64', '--self-contained', 'true', '-p:PublishSingleFile=true'], env=custom_env, cwd=sourcefolder, check=True)
     shutil.copy(f'{sourcefolder}/bin/Release/net6.0/win-x64/publish/{outfile}', outfile)
     if result.returncode == 0:
         print(f'Payload saved to {outfile}')
