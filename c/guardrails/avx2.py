d4649d6359d072a807b33221999a0b2a16522d5f
Bill Chenyoung <bchenyoung@gmail.com>
2026-09-14 16:49:29 +1000

    Added avx2 guardrail for c

---
+++ c/guardrails/avx2.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+class avx2:
+
+    def __init__(self, arguments: dict) -> None:
+        self.name = ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(16))
+
+    def imports(self) -> list[str]:
+        return []
+
+    def compilerOptions(self) -> list[str]:
+        return []
+
+    def transformer(self, shellcodestring: str) -> str:
+        return f'{self.name}();\n\t' + shellcodestring
+
+    def codeblock(self) -> str:
+        return f"""
+void {self.name}()
+{{
+    __m256i a = _mm256_set1_epi32(10);
+}}
+"""
