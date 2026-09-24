1acb0030453c8cd7774ab0bcb327853ab4dc7a4c
Bill Chenyoung <bchenyoung@gmail.com>
2026-09-24 17:34:34 +1000

    Removed unnescessary apicall functions again

--- rs/templates/shellcoderunner.py
+++ rs/templates/shellcoderunner.py
@@ -17,9 +17,6 @@ class shellcoderunner:
     def codeblocks(self) -> str:
         return ''
 
-    def apicalls(self) -> list[str]:
-        return []
-
     def template(self) -> str:
         return """
     {transformers}
